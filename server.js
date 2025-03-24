e187a23df43cc846d5647b3668189c7d74491c89
fanual-software-dev <asfawfanual2003@gmail.com>
2025-03-25 00:17:36 +0300

    session removed

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
-const session = require('express-session')
+
 const mongoose = require('mongoose')
 const DB_URI = process.env.MONGO_URI
 const taskRouter = require('./routes/routes')
@@ -33,16 +33,6 @@ app.use(express.json());
 
 app.use(cors())
 
-app.use(session({
-    secret: process.env.SECRET, // Keep it secret!
-    resave: false, // Prevents unnecessary session updates
-    saveUninitialized: false, // Don't save empty sessions
-    cookie: {
-      maxAge: 1000 * 60 * 60 * 24, // 1 day expiry
-      secure: false, // Set to `true` in production (requires HTTPS)
-      httpOnly: true, // Protects against XSS attacks
-    },
-  }));
 
 app.use(passport.initialize())
 app.use(passport.session())
